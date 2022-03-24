8acdde1d8b70a1992b2d45e94084660e1037e1e1
julianstanley <julianst@mit.edu>
2022-03-24 19:22:51 -0400

    Add a wrapper script to run whole pipeline

---
+++ run_pipeline.m
@@ -0,0 +1,12 @@
+root_dir = pwd;
+terminator_identification_header_random_sequence_git_20200504;
+cd(root_dir);
+clear;
+root_dir = pwd;
+terminator_identification_header_script_v4_git_20200504;
+cd(root_dir);
+clear;
+get_genome_annotations_GC_content_git_20200504;
+parsing_hairpin_data_structures_v2_git_20200504;
+cd("..")
+generate_final_term_data_structure_git_20200504;
